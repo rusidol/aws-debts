059c0b053135939b7cedf763c9001667d5c50d4b
Pavel <rusidol.com@gmail.com>
2024-08-10 09:56:09 +0500

    more setup

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,21 +1,37 @@
 import ujson
 
-from .schemas import DebtsInSchema
+from schemas import DebtsInSchema
 
 
 # import requests
 
 
 def lambda_handler(event, context):
-
-    body = ujson.loads(event["body"])
-
-    if not DebtsInSchema.validate(body):
+    try:
+        body = ujson.loads(event["body"])
+        if not DebtsInSchema.validate(body):
+            return {
+                "statusCode": 422,
+                "body": ujson.dumps(
+                        {
+                            "message": "Unprocessable entry"
+                        }
+                )
+            }
         return {
-            "statusCode": 422,
+                "statusCode": 200,
+                "body": ujson.dumps(
+                        {
+                            "message": "It works!"
+                        }
+                )
+            }
+    except KeyError:
+        return {
+            "statusCode": 400,
             "body": ujson.dumps(
                     {
-                        "message": "Unprocessable entry"
+                        "message": "Bad request"
                     }
             )
         }
